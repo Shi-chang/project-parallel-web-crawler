4ad62664ce7406c8e33ca164ade73aa0426bd252
Shi-chang <shichangye@outlook.com>
2022-09-29 15:59:54 -0700

    Implement run method

--- src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -10,6 +10,7 @@ import com.udacity.webcrawler.json.CrawlerConfiguration;
 import com.udacity.webcrawler.profiler.Profiler;
 import com.udacity.webcrawler.profiler.ProfilerModule;
 
+import java.nio.file.Paths;
 import javax.inject.Inject;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
@@ -19,34 +20,52 @@ import java.util.Objects;
 
 public final class WebCrawlerMain {
 
-  private final CrawlerConfiguration config;
+    private final CrawlerConfiguration config;
 
-  private WebCrawlerMain(CrawlerConfiguration config) {
-    this.config = Objects.requireNonNull(config);
-  }
-
-  @Inject
-  private WebCrawler crawler;
-
-  @Inject
-  private Profiler profiler;
-
-  private void run() throws Exception {
-    Guice.createInjector(new WebCrawlerModule(config), new ProfilerModule()).injectMembers(this);
-
-    CrawlResult result = crawler.crawl(config.getStartPages());
-    CrawlResultWriter resultWriter = new CrawlResultWriter(result);
-    // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
-    // TODO: Write the profile data to a text file (or System.out if the file name is empty)
-  }
-
-  public static void main(String[] args) throws Exception {
-    if (args.length != 1) {
-      System.out.println("Usage: WebCrawlerMain [starting-url]");
-      return;
+    private WebCrawlerMain(CrawlerConfiguration config) {
+        this.config = Objects.requireNonNull(config);
     }
 
-    CrawlerConfiguration config = new ConfigurationLoader(Path.of(args[0])).load();
-    new WebCrawlerMain(config).run();
-  }
+    @Inject
+    private WebCrawler crawler;
+
+    @Inject
+    private Profiler profiler;
+
+    private void run() throws Exception {
+        Guice.createInjector(new WebCrawlerModule(config), new ProfilerModule())
+            .injectMembers(this);
+
+        CrawlResult result = crawler.crawl(config.getStartPages());
+        CrawlResultWriter resultWriter = new CrawlResultWriter(result);
+        // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
+        String resultPath = config.getResultPath();
+        if (!resultPath.isEmpty()) {
+            Path path = Paths.get(resultPath);
+            resultWriter.write(path);
+        } else {
+            Writer outputWriter = new OutputStreamWriter(System.out);
+            resultWriter.write(outputWriter);
+        }
+
+        // TODO: Write the profile data to a text file (or System.out if the file name is empty)
+        if (!config.getProfileOutputPath().isEmpty()) {
+            Path path = Paths.get(config.getProfileOutputPath());
+            profiler.writeData(path);
+        } else {
+            Writer outputWriter = new OutputStreamWriter(System.out);
+            profiler.writeData(outputWriter);
+            outputWriter.flush();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: WebCrawlerMain [starting-url]");
+            return;
+        }
+
+        CrawlerConfiguration config = new ConfigurationLoader(Path.of(args[0])).load();
+        new WebCrawlerMain(config).run();
+    }
 }
